ceb139ef0608199a7a5b058c6215d1447c781db2
GastonSKL <gastonlskamlec@gmail.com>
2024-09-17 15:43:34 -0300

    [add][17/09/2024][Chatbot] Instance of socket.io

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,5 @@
+const socket = io();
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-
 const recognition = new SpeechRecognition();
 
 // recognition.lang = 'en-US';
@@ -15,6 +15,8 @@ recognition.addEventListener('result', (e) => {
     let last = e.results.length - 1;
     let text = e.results[last][0].transcript;
 
+    socket.emit('chat message', text);
     console.log('Confidence: ' + e.results[0][0].confidence);
     
 })
+
